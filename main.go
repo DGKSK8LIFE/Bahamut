b742a79bd84eb222555b2f23a990063f3d735312
Tarek Ali <tarekali15@outlook.com>
2021-01-03 15:27:22 -0600

    project done at this point; ez clap

--- main.go
+++ main.go
@@ -25,18 +25,17 @@ func main() {
 @@@@@@@@@@@@@@@@@@@@@@@~ ,@@@,,0@@@@@@@@@@@@@@@@@@@
 @@@@@@@@@@@@@@@@@@@@@@@@,,@@@@@@@@@@@@@@@@@@@@@@@@@
 ---------------------------------------------------`)
-
 	stats := &util.EthereumStatDump{}
 	stats.Fetch()
 	fmt.Printf(`
-	Height: %d
-	Hash: %s
-	Time: %s
-	Previous Hash: %s	
-	Peer Count: %d
-	High Gas Price: %d
-	Low Gas Price: %d
-	Last Fork Height: %d
-	Last Fork Hash: %s
-	`, stats.Height, stats.Hash, stats.Time, stats.PreviousHash, stats.PeerCount, stats.HighGasPrice, stats.LowGasPrice, stats.LastForkHeight, stats.LastForkHash)
+Height: %d
+Hash: %s
+Time: %s
+Previous Hash: %s	
+Peer Count: %d
+High Gas Price: %d
+Low Gas Price: %d
+Last Fork Height: %d
+Last Fork Hash: %s
+`, stats.Height, stats.Hash, stats.Time, stats.PreviousHash, stats.PeerCount, stats.HighGasPrice, stats.LowGasPrice, stats.LastForkHeight, stats.LastForkHash)
 }
