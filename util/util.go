a8d68866d6af1f9cf5f140bf4c59a51ff2618c48
Tarek Ali <tarekali15@outlook.com>
2021-01-03 14:46:52 -0600

    struct and method declaration

--- util/util.go
+++ util/util.go
@@ -1,5 +1,18 @@
 package util
 
+// EthereumStatDump is a Struct used to fetch JSON blockcypher's REST api
 type EthereumStatDump struct {
-	Address string
+	Height         int64
+	Hash           string
+	Time           string
+	PreviousHash   string
+	PeerCount      int64
+	HighGasPrice   int64
+	LowGasPrice    int64
+	LastForkHeight int64
+	LastForkHash   string
+}
+
+func (dump *EthereumStatDump) fetch() {
+	
 }
