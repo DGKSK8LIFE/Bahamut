cccca05b9a267307c56ea4303cd458e8d92700f4
Tarek Ali <tarekali15@outlook.com>
2021-01-03 14:49:36 -0600

    json object mapping

--- util/util.go
+++ util/util.go
@@ -2,17 +2,17 @@ package util
 
 // EthereumStatDump is a Struct used to fetch JSON blockcypher's REST api
 type EthereumStatDump struct {
-	Height         int64
-	Hash           string
-	Time           string
-	PreviousHash   string
-	PeerCount      int64
-	HighGasPrice   int64
-	LowGasPrice    int64
-	LastForkHeight int64
-	LastForkHash   string
+	Height         int64  `json:"height"`
+	Hash           string `json:"hash"`
+	Time           string `json:"time"`
+	PreviousHash   string `json:"previous_hash"`
+	PeerCount      int64  `json:"peer_count"`
+	HighGasPrice   int64  `json:"high_gas_price"`
+	LowGasPrice    int64  `json:"low_gas_price"`
+	LastForkHeight int64  `json:"last_fork_height"`
+	LastForkHash   string `json:"last_fork_hash"`
 }
 
 func (dump *EthereumStatDump) fetch() {
-	
+
 }
